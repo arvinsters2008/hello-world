fb10fe491d93a50b458ddc5338fcc4b7bb5c0b65
arvinsters2008 <super.bean@yahoo.com>
2019-11-17 02:44:12 +0800

    Tests.py

--- selenium_test/tests.py
+++ selenium_test/tests.py
@@ -1,5 +1,12 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+from django.urls import reverse
 
 
 class SeleniumTest(StaticLiveServerTestCase):
@@ -26,17 +33,67 @@ class SeleniumTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.close()
 
-    def test_login(self):
-        user_name = "arnelsaquilabon@gmail.com"
-        password = "password"
-        self.browser.get(self.live_server_url + "/login/")
-        self.browser.find_element_by_id("username").send_keys(user_name)
-        self.browser.find_element_by_id("password").send_keys(password)
-        self.browser.find_element_by_class_name("btn").click()
+    def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
-        element = self.browser.find_element_by_tag_name('h1')
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
-        self.assertEquals(
-            element.text,
-            "Welcome Arnel"
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
